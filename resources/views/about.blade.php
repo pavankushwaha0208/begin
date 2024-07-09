29845228185afe3c648ff6a12a5ee3f314e65612
pavan kushwaha <pavankush0208@gmail.com>
2024-07-09 12:18:06 +0530

    layout

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+@push('title')
+    <title>about</title>
+@endpush
+
+@section('main-section')
+    
+<h1 class=" text-center">
+    About Page
+</h1>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+@push('title')
+    <title>home</title>
+@endpush
+
+@section('main-section')
+    
+<h1 class=" text-center">
+    Home Page
+</h1>
+
+@endsection
